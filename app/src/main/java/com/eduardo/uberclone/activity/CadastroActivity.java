1512d7dd142f647e9e30ce08df536768fd976bb5
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2019-02-22 14:28:09 -0300

    Salvando dados do usuário no firebase

--- app/src/main/java/com/eduardo/uberclone/activity/CadastroActivity.java
+++ app/src/main/java/com/eduardo/uberclone/activity/CadastroActivity.java
@@ -1,5 +1,6 @@
 package com.eduardo.uberclone.activity;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
@@ -10,11 +11,15 @@ import android.widget.Toast;
 
 import com.eduardo.uberclone.R;
 import com.eduardo.uberclone.config.ConfiguracaoFirebase;
+import com.eduardo.uberclone.helper.UsuarioFirebase;
 import com.eduardo.uberclone.model.Usuario;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class CadastroActivity extends AppCompatActivity {
     private TextInputEditText campoNome, campoEmail, campoSenha;
@@ -61,7 +66,7 @@ public class CadastroActivity extends AppCompatActivity {
         }
     }
 
-    public  void cadastrarUsuario(Usuario usuario){
+    public  void cadastrarUsuario(final Usuario usuario){
         autenticacao = ConfiguracaoFirebase.getFirebaseAuth();
         autenticacao.createUserWithEmailAndPassword(
                 usuario.getEmail(),
@@ -70,7 +75,47 @@ public class CadastroActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()){
-                    Toast.makeText(CadastroActivity.this, "Sucesso ao cadastrar-se", Toast.LENGTH_SHORT).show();
+                    try{
+                        String idUsuario = task.getResult().getUser().getUid();
+                        usuario.setId(idUsuario);
+                        usuario.salvar();
+
+                        //Atualizar nome no UserProfile
+                        UsuarioFirebase.atualizarNomeUsuario(usuario.getNome());
+
+                        /*Redireciona o usuário com base no seu tipo
+                         * Se o usuário for passageiro chama a activity maps,
+                         * se não, chama a activity requisiçoes*/
+                        if (verificaTipoUsuario() == "P"){
+                            startActivity(new Intent(CadastroActivity.this, MapsActivity.class));
+                            finish();
+
+                            Toast.makeText(CadastroActivity.this, "Sucesso ao cadastrar-se", Toast.LENGTH_SHORT).show();
+                        }else{
+                            startActivity(new Intent(CadastroActivity.this, RequisicoesActivity.class));
+                            finish();
+
+                            Toast.makeText(CadastroActivity.this, "Sucesso ao cadastrar Motorista", Toast.LENGTH_SHORT).show();
+                        }
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }else{
+                    String excecao = "";
+                    try{
+                        throw task.getException();
+                    }catch (FirebaseAuthWeakPasswordException e){
+                        excecao = "Digite uma senha mais forte";
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        excecao = "Digite um e-mail válido";
+                    }catch (FirebaseAuthUserCollisionException e){
+                        excecao = "Este e-mail já foi cadastrado";
+                    }catch (Exception e){
+                        excecao = "Erro ao cadastrar usuário: " + e.getMessage();
+                        e.printStackTrace();
+                    }
+
+                    Toast.makeText(CadastroActivity.this, excecao, Toast.LENGTH_SHORT).show();
                 }
             }
         });
--- app/src/main/java/com/eduardo/uberclone/model/Usuario.java
+++ app/src/main/java/com/eduardo/uberclone/model/Usuario.java
@@ -1,5 +1,9 @@
 package com.eduardo.uberclone.model;
 
+import com.eduardo.uberclone.config.ConfiguracaoFirebase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
+
 public class Usuario {
     private String id;
     private String nome;
@@ -8,6 +12,13 @@ public class Usuario {
     private String tipo;
 
     public Usuario() {
+    }
+
+    public void salvar(){
+        DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebaseDatabase();
+        DatabaseReference usuarios = firebaseRef.child("usuarios").child(getId());
+
+        usuarios.setValue(this);
     }
 
     public String getId() {
@@ -34,6 +45,7 @@ public class Usuario {
         this.email = email;
     }
 
+    @Exclude
     public String getSenha() {
         return senha;
     }
